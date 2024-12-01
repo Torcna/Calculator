063c3dccd0b860ace8854823a5f8703b9d517595
Torcna <yuhayo@mail.ru>
2024-12-01 22:51:21 +0300

    add a tutorial

--- src/laba3.cpp
+++ src/laba3.cpp
@@ -1,8 +1,13 @@
 #include "header.hpp"
 int main() {
   string input;
+  std::cout << "HELLO, this calculator supports polynomes as variables. It means, that you have to input polynome "
+            << std::endl;
+  std::cout << "after program asks you to define a variable" << std::endl;
+  std::cout << "Polynome should look like aX^n + ... + bX^l " << std::endl;
+  std::cout << "Where a and b can be long long and double, X can be any char, n and l can be only integer value"
+            << std::endl;
   getline(cin, input);
-
   try {
     Calculator calculator;
     cout << std::endl << calculator.Calculate(input) << std::endl;
